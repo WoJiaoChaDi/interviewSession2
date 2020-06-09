69ed3a496b56afb1f4c3c6e3d74f801e4454741b
wojiaochadi <264835229@qq.com>
2020-06-09 18:03:09 +0800

    Semaphore

---
+++ src/main/java/utils/current/lock/Semaphore/SemaphoreDemo.java
@@ -0,0 +1,41 @@
+package utils.current.lock.Semaphore;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Description
+ *
+ * @author veliger@163.com
+ * @version 1.0
+ * @date 2019-04-13 11:08
+ **/
+public class SemaphoreDemo {
+    public static void main(String[] args) {
+        //模拟3个停车位
+        Semaphore semaphore = new Semaphore(3);
+        //模拟6部汽车
+        for (int i = 1; i <= 6; i++) {
+            new Thread(() -> {
+                try {
+                    System.out.println(Thread.currentThread().getName() + "\t 准备抢车位");
+                    //抢到资源
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName() + "\t 抢到车位");
+                    try {
+                        TimeUnit.SECONDS.sleep(3);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + "\t 停3秒离开车位");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    //释放资源
+                    semaphore.release();
+                }
+            }, String.valueOf(i)).start();
+        }
+    }
+}
+         
