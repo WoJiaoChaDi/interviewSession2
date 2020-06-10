1b2cc8f19eec315bbad3b644b15aa37e83283af2
wojiaochadi <264835229@qq.com>
2020-06-10 14:43:08 +0800

    ArrayBlocking阻塞队列api

--- src/main/java/utils/current/lock/BlockingQueue/ArrayBlockingQueueDemo.java
+++ src/main/java/utils/current/lock/BlockingQueue/ArrayBlockingQueueDemo.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 
-public class BlockingQueueDemo {
+public class ArrayBlockingQueueDemo {
     public static void main(String[] args) {
 
         //抛出异常
