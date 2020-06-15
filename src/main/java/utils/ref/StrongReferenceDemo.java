fa681b5301b9306b5d3d2fe4f2934cad139df4f4
wojiaochadi <264835229@qq.com>
2020-06-15 10:46:44 +0800

    强引用reference

--- src/main/java/utils/ref/StrongReferenceDemo.java
+++ src/main/java/utils/ref/StrongReferenceDemo.java
@@ -4,6 +4,9 @@ public class StrongReferenceDemo {
     public static void main(String[] args) {
         Object obj1 = new Object(); //这样定义的默认就是强引用
         Object obj2 = obj1;//obj2引用赋值
+        System.out.println(obj1);
+        System.out.println(obj2);
+
         obj1 = null;//置空
         System.gc();
         //obj2是强引用，不会被垃圾回收
