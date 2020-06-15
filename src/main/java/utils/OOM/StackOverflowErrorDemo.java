33a080b153be4b0c2137d8f325937083d359c9f5
wojiaochadi <264835229@qq.com>
2020-06-15 14:09:30 +0800

    栈溢出异常

---
+++ src/main/java/utils/OOM/StackOverflowErrorDemo.java
@@ -0,0 +1,11 @@
+package utils.OOM;
+
+public class StackOverflowErrorDemo {
+    public static void main(String[] args) {
+        stackOverflowError();
+    }
+
+    private static void stackOverflowError(){
+        stackOverflowError();
+    }
+}
