56a6fc512af7be46569dfaf352add46615df8c06
wojiaochadi <264835229@qq.com>
2020-06-15 10:48:44 +0800

    软引用reference

--- src/main/java/utils/ref/SoftReferenceDemo.java
+++ src/main/java/utils/ref/SoftReferenceDemo.java
@@ -32,6 +32,8 @@ public class SoftReferenceDemo {
         o1 = null;
         //内存不够用的时候，会自己GC
         //System.gc();
+        System.out.println(o1);
+        System.out.println(softReference.get());
 
         try {
             byte[] bytes = new byte[50 * 1024 * 1024];
@@ -49,16 +51,19 @@ public class SoftReferenceDemo {
         //softRef_Memory_Enough();
         softRef_Memory_Not_Enough();
         //日志：
-//                [GC (Allocation Failure) [PSYoungGen: 1022K->504K(1536K)] 1022K->616K(5632K), 0.0016350 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]
 //        java.lang.Object@7f31245a
 //        java.lang.Object@7f31245a
-//                [GC (Allocation Failure) [PSYoungGen: 953K->496K(1536K)] 1066K->720K(5632K), 0.0007095 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]
-//[GC (Allocation Failure) [PSYoungGen: 496K->504K(1536K)] 720K->736K(5632K), 0.0005691 secs] [Times: user=0.02 sys=0.00, real=0.00 secs]
-//[Full GC (Allocation Failure) [PSYoungGen: 504K->0K(1536K)] [ParOldGen: 232K->594K(4096K)] 736K->594K(5632K), [Metaspace: 3020K->3020K(1056768K)], 0.0083722 secs] [Times: user=0.00 sys=0.00, real=0.01 secs]
-//[GC (Allocation Failure) [PSYoungGen: 0K->0K(1536K)] 594K->594K(5632K), 0.0004826 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]
-//[Full GC (Allocation Failure) [PSYoungGen: 0K->0K(1536K)] [ParOldGen: 594K->577K(4096K)] 594K->577K(5632K), [Metaspace: 3020K->3020K(1056768K)], 0.0083897 secs] [Times: user=0.08 sys=0.00, real=0.01 secs]
-//        Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
 //        null
-//        null    这个内存不够用的时候，软引用也回收了
+//        java.lang.Object@7f31245a
+//                [GC (Allocation Failure) [PSYoungGen: 884K->512K(1536K)] 988K->712K(5632K), 0.0006973 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]
+//[GC (Allocation Failure) [PSYoungGen: 512K->512K(1536K)] 712K->728K(5632K), 0.0008126 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]
+//[Full GC (Allocation Failure) [PSYoungGen: 512K->0K(1536K)] [ParOldGen: 216K->589K(4096K)] 728K->589K(5632K), [Metaspace: 2942K->2942K(1056768K)], 0.0079691 secs] [Times: user=0.00 sys=0.00, real=0.01 secs]
+//[GC (Allocation Failure) [PSYoungGen: 0K->0K(1536K)] 589K->589K(5632K), 0.0003568 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]
+//[Full GC (Allocation Failure) Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
+//                [PSYoungGen: 0K->0K(1536K)] [ParOldGen: 589K->574K(4096K)] 589K->574K(5632K), [Metaspace: 2942K->2942K(1056768K)], 0.0083801 secs] [Times: user=0.05 sys=0.00, real=0.01 secs]
+//        null
+//        at utils.ref.SoftReferenceDemo.softRef_Memory_Not_Enough(SoftReferenceDemo.java:39)
+//        null      //软引用，内存不足的时候进行回收
+//        at utils.ref.SoftReferenceDemo.main(SoftReferenceDemo.java:52)
     }
 }
